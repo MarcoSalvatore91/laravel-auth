b895bb558b6b9ee88a9e031d62dcc103e16f67e5
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-03-28 21:52:28 +0200

    create new file form.php

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,25 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<form class="container" action="{{ route('admin.posts.store') }}" method="POST">
-    @csrf
-    <div class="form-group">
-        <label for="title">Titolo</label>
-        <input type="text" class="form-control" @error('title') is-invalid @enderror id="title" name="title" required minlength="5" maxlength="50" value="{{ old('title') }}">
-        <small class="form-text text-muted">Inserisci un titolo</small>
-    </div>
-    <div class="form-group">
-        <label for="image">Immagine</label>
-        <input type="text" class="form-control" id="image" name="image" value="{{ old('image') }}">
-        <small class="form-text text-muted">Inserisci un'immagine</small>
-    </div>
-    <div class="form-group">
-        <label for="content">Descrizione</label>
-        <textarea class="form-control" @error('content') is-invalid @enderror id="content" name="content" rows="5" required minlength="5">{{ old('content') }}</textarea>
-        <small class="form-text text-muted">Aggiungi una descrizione</small>
-    </div>
-    <div class="d-flex justify-content-end">
-        <button type="submit" class="btn btn-primary">Invia</button>
-    </div>
-</form>
+
+@include('includes.form')
+
 @endsection
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -2,36 +2,6 @@
 
 @section('content')
 
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
+@include('includes.form')
 
-<form class="container" action="{{ route('admin.posts.update', $post->id) }}" method="POST">
-    @csrf
-    @method('PUT')
-    <div class="form-group">
-        <label for="title">Titolo</label>
-        <input type="text" class="form-control" @error('title') is-invalid @enderror id="title" name="title" required minlength="5" maxlength="50" value="{{ old('title', $post->title) }}">
-        <small class="form-text text-muted">Inserisci un titolo</small>
-    </div>
-    <div class="form-group">
-        <label for="image">Immagine</label>
-        <input type="text" class="form-control" id="image" name="image" value="{{ old('image', $post->image) }}">
-        <small class="form-text text-muted">Inserisci un'immagine</small>
-    </div>
-    <div class="form-group">
-        <label for="content">Descrizione</label>
-        <textarea class="form-control" id="content" @error('content') is-invalid @enderror name="content" rows="5" required minlength="5">{{ old('content', $post->content) }}</textarea>
-        <small class="form-text text-muted">Aggiungi una descrizione</small>
-    </div>
-    <div class="d-flex justify-content-end">
-        <button type="submit" class="btn btn-primary">Invia</button>
-    </div>
-</form>
 @endsection
